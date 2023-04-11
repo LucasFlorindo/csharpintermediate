938272aaceaeca681088a17539903d2d4f044e9d
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-11 11:09:43 -0300

    Exercício de enumerações e composição

---
+++ exenumsecomp/exenumsecomp/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exenumsecomp.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
